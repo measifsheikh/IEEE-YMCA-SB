7acdd27c93c32abd8f16bc480551a8d41f9c1d94
measifsheikh <112983817+measifsheikh@users.noreply.github.com>
2024-02-23 00:38:10 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -1,75 +1,109 @@
-document.getElementsByClassName("light")[0].click(1);
+var verticalposition;
 
-    var delayInMilliseconds = 1500;
+document.body.addEventListener("wheel", () => {
+  // Get the total available scroll length of the document
+  var trackLength = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+  // Calculate how much the user has scrolled percentage-wise
+  var pctScrolled = (window.scrollY / trackLength) * 100;
+
+ 
+
+  //console.log("Vertical position:", window.scrollY);
+  console.log("Scroll percentage:", pctScrolled);
+
+  if (pctScrolled > 12 & pctScrolled < 20){
+
+    var delayInMilliseconds = 500;
 
     setTimeout(function () {
-
+    
       const counters = document.querySelectorAll('.count');
       const speed = 10;
       counters.forEach((counter) => {
         const updateCount = () => {
           const target = parseInt(counter.getAttribute('data-target'));
           const count = parseInt(counter.innerText);
-          const increment = Math.trunc(target / speed);
+       const increment = Math.trunc((target) / speed);
 
-          if (count < target) {
+
+          if (count < target ) {
+            if (target < 50){
+                counter.innerText = count + 1;
+                setTimeout(updateCount, 500);
+            }
+else{
             counter.innerText = count + increment;
-            setTimeout(updateCount, 30);
+            setTimeout(updateCount, 500);
+
+
+}
           } else {
             counter.innerText = target;
           }
         };
         updateCount();
       });
-
+    
     }, delayInMilliseconds);
 
- 
-
-$( ".light" ).on("click", function() {
-  if( $( "#nav11" ).hasClass( "dark" )) {
-      $( "#nav11" ).removeClass( "dark" );
-      
-  } else {
-      $( "#nav11" ).addClass( "dark" );
-  }
-  if( $( "#about" ).hasClass( "dark-about" )) {
-      $( "#about" ).removeClass( "dark-about" );
-      
-  } else {
-      $( "#about" ).addClass( "dark-about" );
-  }
-
-  if( $( "#project" ).hasClass( "dark-project" )) {
-      $( "#project" ).removeClass( "dark-project" );
-      
-  } else {
-      $( "#project" ).addClass( "dark-project" );
-  }
-  
-  if( $( "#Event" ).hasClass( "dark-event" )) {
-      $( "#Event" ).removeClass( "dark-event" );
-      
-  } else {
-      $( "#Event" ).addClass( "dark-event" );
-  }
-  if( $( "#Membership" ).hasClass( "dark-Membership" )) {
-      $( "#Membership" ).removeClass( "dark-Membership" );
-      
-  } else {
-      $( "#Membership" ).addClass( "dark-Membership" );
-  }
-  if( $( "#glimpse-tag-1" ).hasClass( "dark-glimpse-tag-1" )) {
-      $( "#glimpse-tag-1" ).removeClass( "dark-glimpse-tag-1" );
-      
-  } else {
-      $( "#glimpse-tag-1" ).addClass( "dark-glimpse-tag-1" );
-  }
-  if( $( "#glimpse-tag" ).hasClass( "dark-glimpse-tag" )) {
-      $( "#glimpse-tag" ).removeClass( "dark-glimpse-tag" );
-      $( ".change" ).text( "Darkness awaits ❦." );
-  } else {
-      $( "#glimpse-tag" ).addClass( "dark-glimpse-tag" );
-      $( ".change" ).text( "Let there be light 🪄" );
   }
 });
+
+
+
+
+
+
+
+
+
+$( ".light" ).on("click", function() {
+    if( $( "#nav11" ).hasClass( "dark" )) {
+        $( "#nav11" ).removeClass( "dark" );
+        
+    } else {
+        $( "#nav11" ).addClass( "dark" );
+    }
+    if( $( "#about" ).hasClass( "dark-about" )) {
+        $( "#about" ).removeClass( "dark-about" );
+        
+    } else {
+        $( "#about" ).addClass( "dark-about" );
+    }
+  
+    if( $( "#project" ).hasClass( "dark-project" )) {
+        $( "#project" ).removeClass( "dark-project" );
+        
+    } else {
+        $( "#project" ).addClass( "dark-project" );
+    }
+    
+    if( $( "#Event" ).hasClass( "dark-event" )) {
+        $( "#Event" ).removeClass( "dark-event" );
+        
+    } else {
+        $( "#Event" ).addClass( "dark-event" );
+    }
+    if( $( "#Membership" ).hasClass( "dark-Membership" )) {
+        $( "#Membership" ).removeClass( "dark-Membership" );
+        
+    } else {
+        $( "#Membership" ).addClass( "dark-Membership" );
+    }
+    if( $( "#glimpse-tag-1" ).hasClass( "dark-glimpse-tag-1" )) {
+        $( "#glimpse-tag-1" ).removeClass( "dark-glimpse-tag-1" );
+        
+    } else {
+        $( "#glimpse-tag-1" ).addClass( "dark-glimpse-tag-1" );
+    }
+    if( $( "#glimpse-tag" ).hasClass( "dark-glimpse-tag" )) {
+        $( "#glimpse-tag" ).removeClass( "dark-glimpse-tag" );
+        $( ".change" ).text( "Darkness awaits ❦." );
+    } else {
+        $( "#glimpse-tag" ).addClass( "dark-glimpse-tag" );
+        $( ".change" ).text( "Let there be light 🪄" );
+    }
+  });
+
+  document.getElementsByClassName("light")[0].click(1);
